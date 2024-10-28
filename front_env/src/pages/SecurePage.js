be67c99fbfbbfe7d88a89d5682868b52689479be
zakergfx <zakergfx@gmail.com>
2024-10-28 19:50:39 +0100

    edited security page

--- front_env/src/pages/SecurePage.js
+++ front_env/src/pages/SecurePage.js
@@ -5,7 +5,13 @@ function SecurePage() {
         <div className="SecurePage">
             <div className="Content">
                 <h1>How it it secure ?</h1>
-                <p>It is not.</p>
+                <ul>
+                    <li>Secrets have a <b>limited lifetime</b>, 7 days is the maximum</li>
+                    <li>By default secrets are automatically <b>removed after opened once</b></li>
+                    <li>The transmission of data is <b>encrypted using HTTPS</b></li>
+                    <li>The secrets are stored using <b>AES encryption </b></li>
+                    <li>We do <b>not keep logs</b> about shared data</li>
+                </ul>
             </div >
         </div>
 
