03e4b23e6565f4010b7d7f64a363e0df4d8df583
zakergfx <zakergfx@gmail.com>
2024-10-28 18:48:05 +0100

    removed useless info

--- back_env/source/backend/settings.py
+++ back_env/source/backend/settings.py
@@ -38,9 +38,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nrgj_&s%84njo%-@4$jvfh3kl@&!9_9vcf0ee_hwc=#yqxohnf'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
