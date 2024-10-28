5a0bd0f1c0244db1f70f63697e5c962037c21c4e
zakergfx <zakergfx@gmail.com>
2024-10-28 18:57:08 +0100

    changed secret key

--- back_env/source/backend/settings.py
+++ back_env/source/backend/settings.py
@@ -38,6 +38,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
